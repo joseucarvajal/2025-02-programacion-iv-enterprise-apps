a70fa3fabfd9af838b8b3bcc44cc6d0165e743ad
joseucarvajal <joseucarvajal@gmail.com>
2025-08-12 20:54:49 -0500

    first commit

---
+++ v1_ecommerce_variables_operadores_output/main.py
@@ -0,0 +1,30 @@
+#Variables
+nombre_producto1 = "Laptop"
+precio_producto1 = 1000
+cantidad_producto1 = 2
+#operadores (en este caso multiplicacion)
+total_producto1 = precio_producto1 * cantidad_producto1
+
+
+#Variables
+nombre_producto2 = "Mouse"
+precio_producto2 = 100
+cantidad_producto2 = 3
+#operadores (en este caso multiplicacion)
+total_producto2 = precio_producto2 * cantidad_producto2
+
+#Hay muchas formas de hacer las mismas operaciones
+#total_compra = (precio_producto1 * cantidad_producto1) + (precio_producto2 * cantidad_producto2)
+#total_compra = total_producto1 + (precio_producto2 * cantidad_producto2)
+total_compra = total_producto1 + total_producto2
+
+#output
+#print(...): esto es una instruccion que le estamos indicando al programa
+print(f"El total de la compra es: {total_compra}")
+
+
+
+
+
+
+
