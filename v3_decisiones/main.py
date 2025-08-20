683e31e48e9f4cca9e8081eb22838c2f78030f01
joseucarvajal <joseucarvajal@gmail.com>
2025-08-19 21:11:31 -0500

    Clase de decisiones

---
+++ v3_decisiones/main.py
@@ -0,0 +1,47 @@
+#Variables
+#inputs
+nombre_producto1 = input("Ingrese el nombre del producto 1: ")
+precio_producto1 = float(input("Ingrese el precio del producto 1: "))
+
+#decisiones
+if (precio_producto1 < 0):
+    print("ERROR: El precio del producto no puede ser menor o igual a 0")
+else:
+    cantidad_producto1 = input("Ingrese el cantidad del producto 1: ")
+    cantidad_producto1 = int(cantidad_producto1)
+    if(cantidad_producto1 < 0):
+        print("ERROR: La cantidad del producto no puede ser menor o igual a 0")
+    else:
+        #operadores (en este caso multiplicacion)
+        total_producto1 = precio_producto1 * cantidad_producto1
+
+        #Variables
+        nombre_producto2 = input("Ingrese el nombre del producto 2: ")
+        precio_producto2 = float(input("Ingrese el precio del producto 2: "))
+        if (precio_producto2 < 0):
+            print("ERROR: El precio del producto no puede ser menor o igual a 0")
+        else:
+            cantidad_producto2 = input("Ingrese el cantidad del producto 2: ")
+            cantidad_producto2 = int(cantidad_producto2)
+            if(cantidad_producto2 < 0):
+                print("ERROR: La cantidad del producto no puede ser menor o igual a 0")
+            else:
+                #operadores (en este caso multiplicacion)
+                total_producto2 = precio_producto2 * cantidad_producto2
+
+
+                #Hay muchas formas de hacer las mismas operaciones
+                #total_compra = (precio_producto1 * cantidad_producto1) + (precio_producto2 * cantidad_producto2)
+                #total_compra = total_producto1 + (precio_producto2 * cantidad_producto2)
+                total_compra = total_producto1 + total_producto2
+
+                #output
+                #print(...): esto es una instruccion que le estamos indicando al programa
+                print(f"El total de la compra es: {total_compra}")
+
+
+
+
+
+
+
